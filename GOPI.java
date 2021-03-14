0451fcdb178ca5c1d35e6570f4ee6bf4ffc223f0
NijuNiranjan <77233057+NijuNiranjan@users.noreply.github.com>
2021-03-14 16:08:25 +0530

    Add files via upload

---
+++ GOPI.java
@@ -0,0 +1,17 @@
+public class GOPI
+{
+	public static void main(String[] args) {
+	    for(int rows=0;rows<4;rows++)
+	    {
+	        for(int col=0;col<4;col++)
+	        {
+	            if(rows==0 || rows==3 || col==0 || col==3)
+	           System.out.print("* ");
+				else
+					System.out.print("  ");
+			}
+			System.out.print("\n");
+		}
+	}
+}
+ANR SIFTWARE
